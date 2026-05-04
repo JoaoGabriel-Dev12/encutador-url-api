4e46fb54bea71875a0b9e319fddd21d2e3676d96
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-05-03 22:08:53 -0300

    feat: CorsConfig

---
+++ src/main/java/com/joaogabriel/dev/encurtador/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.joaogabriel.dev.encurtador.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+		.allowedHeaders("*");
+	}
+
+}
